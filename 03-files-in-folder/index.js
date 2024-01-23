af349eb784d5d5fee7b02d4cefb9af14017177a7
Natalia <lazorenko.natasha@yandex.ru>
2024-01-24 00:10:31 +0800

    init: start and do 3 task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const pathFolder = path.join(__dirname, 'secret-folder');
+
+function writeParamsOfFile(filePath, fileExtension, fileName) {
+fs.stat(filePath, (error, stats) => {
+    if (error) console.log(error.message);
+    console.log(`${fileName} - ${fileExtension} - ${stats.size / 1000}kb`);
+    return stats.size;
+});
+}
+
+fs.readdir(pathFolder, { withFileTypes: true }, (error, files) => {
+    if (error) {
+        console.log(error.message);
+    }
+    files.forEach((file) => {
+      if (file.isFile()) {
+        const filePath = path.join(pathFolder, file.name);
+        const fileName = path.parse(filePath).name;
+        const fileExtension = path.extname(filePath).slice(1);
+        writeParamsOfFile(filePath, fileExtension, fileName);
+      }
+    });
+});
